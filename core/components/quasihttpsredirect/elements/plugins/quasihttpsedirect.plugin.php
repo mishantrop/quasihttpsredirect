8a525717cc3b1aa387ce9e019050a44b66c6696e
Mikhail <mishantrop@users.noreply.github.com>
2020-06-06 23:15:38 +0300

    Simplify

--- core/components/quasihttpsredirect/elements/plugins/quasihttpsedirect.plugin.php
+++ core/components/quasihttpsredirect/elements/plugins/quasihttpsedirect.plugin.php
@@ -1,32 +1,30 @@
 <?php
 /**
  * @author <https://quasi-art.ru>
- * @version 1.0
+ * @version 1.1
  * 2017
  */
 
 $debug = false;
-if ($debug) {
-	$modx->log(xPDO::LOG_LEVEL_ERROR, 'Event: '.$modx->event->name);
-}
+$contexts = [ 'web' ];
+
 switch ($modx->event->name) {
-    case 'OnHandleRequest':
-        // $modx->setDebug(E_ALL & ~E_NOTICE);
-        // $modx->setLogLevel(modX::LOG_LEVEL_DEBUG);
-        if ($modx->context->key == 'web') {
-            $uri = $modx->getOption('REQUEST_URI', $_SERVER, '/');
-            $host = $modx->getOption('HTTP_HOST', $_SERVER, '');
-            $https = $modx->getOption('HTTPS', $_SERVER, '');
-            $scheme = $modx->getOption('REQUEST_SCHEME', $_SERVER, 'http');
-            $httpsRequestUri = 'https://'.$host.$uri;
-            if ($debug) {
-        		$modx->log(xPDO::LOG_LEVEL_ERROR, 'Redirect to '.$httpsRequestUri);
-            }
-            if ($scheme == 'http' && $https == '') {
-                $modx->sendRedirect($httpsRequestUri, array('responseCode' => 'HTTP/1.1 301 Moved Permanently'));
-            }
-        }
-        break;
-    default:
-        break;
+  case 'OnHandleRequest':
+    if (!in_array($modx->context->key, $contexts)) {
+      break;
+    }
+    $uri = $modx->getOption('REQUEST_URI', $_SERVER, '/');
+    $host = $modx->getOption('HTTP_HOST', $_SERVER, '');
+    $https = $modx->getOption('HTTPS', $_SERVER);
+    $scheme = $modx->getOption('REQUEST_SCHEME', $_SERVER, 'http');
+    $httpsUri = "https://$host$uri";
+    if ($scheme === 'http' && !$https) {
+      if ($debug) {
+        $modx->log(xPDO::LOG_LEVEL_ERROR, "Redirecting to $httpsUri");
+      }
+      $modx->sendRedirect($httpsUri, [ 'responseCode' => 'HTTP/1.1 301 Moved Permanently' ]);
+    }
+  break;
+  default:
+    break;
 }
